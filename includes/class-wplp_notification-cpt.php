fb66107b3c1106ca5cfd5d87299c8a2c58228563
ranju58 <ranjubhusal01@gmail.com>
2024-07-17 17:05:55 +0545

    affiliate % updated in user meta

--- includes/class-wplp_notification-cpt.php
+++ includes/class-wplp_notification-cpt.php
@@ -7,6 +7,10 @@ class Wplp_notification_CPT
         add_action('save_post', array($this, 'save_tickets_meta_box'));
         add_action('admin_enqueue_scripts', array($this, 'tickets_admin_scripts'));
         add_action('do_meta_boxes', array($this, 'remove_default_custom_fields'));
+
+        // Add custom column to the All Users page
+        add_filter('manage_users_columns', array($this, 'add_affiliate_percentage_column'));
+        add_action('manage_users_custom_column', array($this, 'show_affiliate_percentage_column'), 10, 3);
     }
 
     public function add_tickets_meta_boxes()
@@ -37,33 +41,33 @@ class Wplp_notification_CPT
         $client_id = get_post_meta($post->ID, 'client_id', true);
         $users = get_users();
 ?>
-        <label for="task_status">Task Status:</label>
-        <select name="task_status" id="task_status">
-            <option value="Early" <?php selected($task_status, 'Early'); ?>>Early</option>
-            <option value="Contacted" <?php selected($task_status, 'Contacted'); ?>>Contacted</option>
-            <option value="Awaiting Response" <?php selected($task_status, 'Awaiting Response'); ?>>Awaiting Response</option>
-            <option value="Converted" <?php selected($task_status, 'Converted'); ?>>Converted</option>
-            <option value="Completed" <?php selected($task_status, 'Completed'); ?>>Completed</option>
-        </select>
-        <br>
-        <label for="affiliate_id">Affiliate ID:</label>
-        <select name="affiliate_id" id="affiliate_id">
-            <option value=""><?php _e('Select Affiliate', 'wplp_notification'); ?></option>
-            <?php foreach ($users as $user) { ?>
-                <option value="<?php echo $user->ID; ?>" <?php selected($affiliate_id, $user->ID); ?>>
-                    <?php echo $user->display_name; ?></option>
-            <?php } ?>
-        </select>
-        <br>
-        <label for="client_id">Client ID:</label>
-        <select name="client_id" id="client_id">
-            <option value=""><?php _e('Select Client', 'wplp_notification'); ?></option>
-            <?php foreach ($users as $user) { ?>
-                <option value="<?php echo $user->ID; ?>" <?php selected($client_id, $user->ID); ?>>
-                    <?php echo $user->display_name; ?></option>
-            <?php } ?>
-        </select>
-    <?php
+<label for="task_status">Task Status:</label>
+<select name="task_status" id="task_status">
+    <option value="Early" <?php selected($task_status, 'Early'); ?>>Early</option>
+    <option value="Contacted" <?php selected($task_status, 'Contacted'); ?>>Contacted</option>
+    <option value="Awaiting Response" <?php selected($task_status, 'Awaiting Response'); ?>>Awaiting Response</option>
+    <option value="Converted" <?php selected($task_status, 'Converted'); ?>>Converted</option>
+    <option value="Completed" <?php selected($task_status, 'Completed'); ?>>Completed</option>
+</select>
+<br>
+<label for="affiliate_id">Affiliate ID:</label>
+<select name="affiliate_id" id="affiliate_id">
+    <option value=""><?php _e('Select Affiliate', 'wplp_notification'); ?></option>
+    <?php foreach ($users as $user) { ?>
+    <option value="<?php echo $user->ID; ?>" <?php selected($affiliate_id, $user->ID); ?>>
+        <?php echo $user->display_name; ?></option>
+    <?php } ?>
+</select>
+<br>
+<label for="client_id">Client ID:</label>
+<select name="client_id" id="client_id">
+    <option value=""><?php _e('Select Client', 'wplp_notification'); ?></option>
+    <?php foreach ($users as $user) { ?>
+    <option value="<?php echo $user->ID; ?>" <?php selected($client_id, $user->ID); ?>>
+        <?php echo $user->display_name; ?></option>
+    <?php } ?>
+</select>
+<?php
     }
 
     public function display_details_meta_box($post)
@@ -74,14 +78,15 @@ class Wplp_notification_CPT
         $task_amt_total = get_post_meta($post->ID, 'task_amt_total', true);
         $affiliate_cut = get_post_meta($post->ID, 'affiliate_cut', true);
     ?>
-        <label for="task_amt_converted">Task Amount Converted:</label>
-        <input type="number" name="task_amt_converted" id="task_amt_converted" min="0" value="<?php echo esc_attr($task_amt_converted); ?>">
-        <br>
-        <label for="task_amt_total">Task Amount Total:</label>
-        <input type="number" name="task_amt_total" id="task_amt_total" min="0" value="<?php echo esc_attr($task_amt_total); ?>">
-        <br>
-        <label for="affiliate_cut">Affiliate Cut:</label>
-        <input type="number" name="affiliate_cut" id="affiliate_cut" min="0" value="<?php echo esc_attr($affiliate_cut); ?>">
+<label for="task_amt_converted">Task Amount Converted:</label>
+<input type="number" name="task_amt_converted" id="task_amt_converted" min="0"
+    value="<?php echo esc_attr($task_amt_converted); ?>">
+<br>
+<label for="task_amt_total">Task Amount Total:</label>
+<input type="number" name="task_amt_total" id="task_amt_total" min="0" value="<?php echo esc_attr($task_amt_total); ?>">
+<br>
+<label for="affiliate_cut">Affiliate Cut:</label>
+<input type="number" name="affiliate_cut" id="affiliate_cut" min="0" value="<?php echo esc_attr($affiliate_cut); ?>">
 <?php
     }
 
@@ -121,17 +126,31 @@ class Wplp_notification_CPT
         if (isset($_POST['client_id'])) {
             update_post_meta($post_id, 'client_id', intval($_POST['client_id']));
         }
-        // Update affiliate_cut for special statuses
-        if (in_array($_POST['task_status'], array('Converted', 'Completed'))) {
-            $task_amt_total = floatval(get_post_meta($post_id, 'task_amt_total', true));
-
-            // Calculate affiliate cut
-            $affiliate_cut = $task_amt_total * 0.25;
-
-            // Update affiliate_cut meta field
-            update_post_meta($post_id, 'affiliate_cut', $affiliate_cut);
+        if (isset($_POST['affiliate_cut'])) {
+            update_post_meta($post_id, 'affiliate_cut', sanitize_text_field($_POST['affiliate_cut']));
         }
 
+        // Calculate affiliate cut for special statuses
+        if (in_array($_POST['task_status'], array('Converted', 'Completed'))) {
+            $task_amt_total = floatval(get_post_meta($post_id, 'task_amt_total', true));
+            $affiliate_cut = $task_amt_total * 0.25;
+            update_post_meta($post_id, 'affiliate_cut', $affiliate_cut);
+        }
+        // Calculate and save affiliate percentage for the affiliate ID
+        if (isset($_POST['affiliate_id'])) {
+            $affiliate_id = intval($_POST['affiliate_id']);
+            $task_amt_total = floatval(get_post_meta($post_id, 'task_amt_total', true));
+            $affiliate_cut = floatval(get_post_meta($post_id, 'affiliate_cut', true));
+
+            if ($task_amt_total > 0) {
+                $affiliate_percentage = ($affiliate_cut / $task_amt_total) * 100;
+            } else {
+                $affiliate_percentage = 0;
+            }
+
+            update_user_meta($affiliate_id, 'affiliate_percentage', $affiliate_percentage);
+        }
+        
         // Add a notification when task_status is changed
         if (isset($_POST['task_status']) && $_POST['task_status'] !== $previous_status) {
             $this->add_notification($post_id, 'task_status', sanitize_text_field($_POST['task_status']));
@@ -157,6 +176,21 @@ class Wplp_notification_CPT
     public function remove_default_custom_fields()
     {
         remove_meta_box('postcustom', 'ticket', 'normal');
+    }
+
+    public function add_affiliate_percentage_column($columns)
+    {
+        $columns['affiliate_percentage'] = __('Affiliate %', 'wplp_notification');
+        return $columns;
+    }
+
+    public function show_affiliate_percentage_column($value, $column_name, $user_id)
+    {
+        if ('affiliate_percentage' == $column_name) {
+            $affiliate_percentage = get_user_meta($user_id, 'affiliate_percentage', true);
+            return $affiliate_percentage ? $affiliate_percentage . '%' : __('N/A', 'wplp_notification');
+        }
+        return $value;
     }
 
     public function add_notification($post_id, $meta_key, $meta_value)
